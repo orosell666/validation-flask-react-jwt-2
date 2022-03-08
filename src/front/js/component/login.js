189b36b61e32b38520c9eb0fdc18d3c665d8b04d
orosell666 <o.rosell.ribas@gmail.com>
2022-03-08 22:04:57 +0000

    useState y tal...ups

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 // falta useState para guardar la info del mail i passwprd
 // funcion que se ejecute cuando haga onclick en el boton login=> hara un fetch a mi api (url del insomnia)
 const Login = () => {
-  return (
-    <div className="text-center mt-5">
-      <h1>Login</h1>
-      <div class="input-group input-group-sm mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-sm">
-          Email
-        </span>
-        <input
-          type="text"
-          class="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing-sm"
-        />
-      </div>
-      <div class="input-group input-group-sm mb-3">
-        <span class="input-group-text" id="inputGroup-sizing-sm">
-          Password
-        </span>
-        <input
-          type="text"
-          class="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing-sm"
-        />
-      </div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-      <div>
-        <button type="button" class="btn btn-dark">
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email </label>
+          <input
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-dark mt-30,block"
+          disabled={!validateForm()}
+        >
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 };
