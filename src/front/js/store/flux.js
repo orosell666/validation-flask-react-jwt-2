b4c6c172ef98e417dec65aadc7e4c16a87bb7ccc
orosell666 <o.rosell.ribas@gmail.com>
2022-07-06 14:57:10 +0000

    falla el token

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,13 +13,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 				// fetching data from the backend
 				console.log(email, password);
 
-				fetch(process.env.BACKEND_URL + "/token", {
+				fetch(process.env.BACKEND_URL + "/api/token", {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({ email: email, password: password }),
 				})
 					.then(resp => resp.json())
-					.then(data => setStore({ respuesta: data }))
+					.then(data => setStore({ respuesta: data }),
+						localStorage.setItem("token", data.token),
+						localStorage.setItem("user_id", data.user_id))
 					.catch(error => console.log("Error loading message from backend", error));
 			},
 
